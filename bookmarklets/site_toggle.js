c4db27c719e90b1869ebd1478d5c844e8d878548
davidvarney <varney@mwesales.com>
2017-04-26 13:57:02 -0400

    Adds a bookmarklet for switching between staging and production sites.

---
+++ bookmarklets/site_toggle.js
@@ -0,0 +1,25 @@
+var protocol = window.location.protocol;
+var current_url = window.location.host;
+var uri = window.location.pathname;
+var params = window.location.search.substring(1);
+//console.log("Protocol: " + protocol);// USED FOR TESTING
+//console.log("URL: " + current_url);// USED FOR TESTING
+//console.log("URI: " + uri);// USED FOR TESTING
+//console.log("Params: " + params);// USED FOR TESTING
+var new_url = "";
+switch (current_url) {
+    case 'www.staging.westinghouseportablepower.com':
+        new_url = "//www.westinghouseportablepower.com";
+    break;
+    case 'www.westinghouseportablepower.com':
+        new_url = "//www.staging.westinghouseportablepower.com";
+    break;
+    case 'www.staging.tracksandtires.com':
+        new_url = "//www.tracksandtires.com";
+    break;
+    case 'www.tracksandtires.com':
+        new_url = "//www.staging.tracksandtires.com";
+    break;
+}
+
+window.location = protocol + new_url + uri + "?" + params;
